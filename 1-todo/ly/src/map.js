a399cc138aa89803cd7d97b51cf2d326287650a5
袁 <358881498@qq.com>
2017-10-29 22:35:24 +0800

    测试2

--- 1-todo/ly/src/map.js
+++
@@ -1,4 +0,0 @@
-const s = [1, 2, 3, 4, 5];
-let r = s.filter(e => e % 2 === 1)
-  .map(e => e * e)
-  .reduce((acc, e) => acc + e, 0);
--- 1-todo/zhaodanfeng/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import TodoListContainer from './Component/TodoListContainer.jsx';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<TodoListContainer />, document.getElementById('root'));
-registerServiceWorker();
