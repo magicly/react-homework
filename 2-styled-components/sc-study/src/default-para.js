f4d0ef1aea703147fe60d82ac5bfc3d2a88a44d7
magicly <magicly007@gmail.com>
2017-11-02 19:44:26 +0800

    es6

---
+++ 2-styled-components/sc-study/src/default-para.js
@@ -0,0 +1,13 @@
+function f(a, b) {
+  // a = a || 1;
+  // b = b || 2;
+  if (a === undefined) {
+    a = 1;
+  }
+  if (b === undefined) {
+    b = 2;
+  }
+  return a + b;
+}
+
+console.log(f(1));
