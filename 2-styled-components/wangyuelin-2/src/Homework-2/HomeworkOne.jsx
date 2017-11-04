f898765ec3180af77437d525ab6aea0aabfd3f32
385116560@qq.com <358116560@qq.com>
2017-11-05 00:18:18 +0800

    样式调整完成，删除css文件

--- 2-styled-components/wangyuelin-2/src/Homework-2/HomeworkOne.jsx
+++ 2-styled-components/wangyuelin-2/src/Homework-2/HomeworkOne.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import './Homework.css';
 import _ from 'lodash';
-import styled from 'styled-components';
+import styled, { injectGlobal } from 'styled-components';
 
 /**
  * 输入框 控制
@@ -242,7 +241,7 @@ class Todo extends Component {
 
     render() {
         return (
-            <div className="todoapp">
+            <TodoApp>
                 <h1>todos</h1>
                 <InputView checked={this.state.prevCheck} onChange={this.changeCheck} />
                 <CustomList
@@ -256,17 +255,17 @@ class Todo extends Component {
                     deleteChecked={this.deleteChecked}
                     showWays={this.state.showWays}
                 />
-            </div>
+            </TodoApp>
         );
     }
 }
 
 const InputView = props => {
     return (
-        <div className="new-todo">
-            <input  type="checkbox" checked={props.checked} onChange={() => props.onChange()} />
+        <NewTodo>
+            <ToggleAll type="checkbox" checked={props.checked} onChange={() => props.onChange()} />
             <input className="edit" type="text" placeholder="What needs to be done?" />
-        </div>
+        </NewTodo>
     );
 }
 
@@ -277,11 +276,14 @@ const CustomList = props => {
     if (arr.data.length > 0) {
         list = arr.data.map((item, index) => {
             return (
-                <div className="todo-list" key={index}>
-                    <input type="checkbox" checked={item.isChecked} onChange={() => props.checkOne(index)} />
-                    <span className={item.isChecked ? 'checked' : ''}>{item.name}</span>
+                <CustomBox key={index}>
+                    <HandleCheck>
+                        <CustomIpt type="checkbox" checked={item.isChecked} onChange={() => props.checkOne(index)} ></CustomIpt>
+                    </HandleCheck>
+                    <CustomText className={item.isChecked ? 'checked text' : 'text'}>{item.name}</CustomText>
                     <button onClick={() => props.deleteOne(index)}></button>
-                </div>
+                </CustomBox>
+
             )
         });
     }
@@ -306,6 +308,47 @@ const Footer = props => {
     );
 }
 
+//全局样式
+injectGlobal`
+html,
+body {
+	margin: 0;
+	padding: 0;
+}
+
+button {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	background: none;
+	font-size: 100%;
+	vertical-align: baseline;
+	font-family: inherit;
+	font-weight: inherit;
+	color: inherit;
+	-webkit-appearance: none;
+	appearance: none;
+	-webkit-font-smoothing: antialiased;
+	-moz-font-smoothing: antialiased;
+	font-smoothing: antialiased;
+}
+
+body {
+	font: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	line-height: 1.4em;
+	background: #f5f5f5;
+	color: #4d4d4d;
+	min-width: 230px;
+	max-width: 550px;
+	margin: 0 auto;
+	-webkit-font-smoothing: antialiased;
+	-moz-font-smoothing: antialiased;
+	font-smoothing: antialiased;
+	font-weight: 300;
+}
+`
+
+
 const Span = styled.span`
     zoom: 1;
     float:left;
@@ -314,31 +357,6 @@ const Span = styled.span`
     text-decoration: none;
     color:inherit;
     text-align: center;
-`
-const FilterBtn = Span.extend`
-    margin: 1px;
-    text-decoration: none;
-    border: 1px solid transparent;
-    padding: 3px 10px;
-    vertical-align: baseline;
-    outline: none;
-    cursor: pointer;
-    text-align: center;
-    border-radius: 3px;
-
-    // box-shadow: 1px 1px 1px rgba(0,0,0,0.1);
-    // :hover{
-    //     border:1px solid #e8e8e8;  
-    // }
-
-    span.selected,
-    :hover {
-        border-color: rgba(175, 47, 47, 0.1);
-    }
-    
-    selected {
-        border-color: rgba(175, 47, 47, 0.2);
-    }
 `
 /**带样式的footer */
 const FooterWithStyle = styled(Footer) `
@@ -381,6 +399,7 @@ const FooterWithStyle = styled(Footer) `
     	border-color: rgba(175, 47, 47, 0.2);
     }
 `
+
 /**删除勾选按钮 */
 const DeleteBtn = Span.extend`
     margin-left: 50px;
@@ -402,4 +421,196 @@ const LeftItem = Span.extend`
     outline: none;
     text-align: center;
 `
+
+// list-box
+const CustomBox = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: nowrap;
+    border: none;
+    padding: 12px 0;
+    background: rgba(0, 0, 0, 0.003);
+    box-shadow: inset 0 1px 1px rgba(0,0,0,0.03);
+
+    // del Btn
+    :hover button{
+        opacity: 1;
+    }
+    button{
+        opacity: 0;
+        border:0;
+        outline:none;
+        cursor: pointer;
+        background: none;
+        width: 40px;
+        height: 40px;
+        font-size: 30px;
+        color: #cc9a9a;
+        transition: color 0.2s ease-out;
+    }
+    button:hover {
+        color: #af5b5e;
+    }
+    button:after {
+        content: '×';
+    }
+`
+
+// checkbox-box
+const HandleCheck = styled.span`
+    padding: 0;
+    width: 50px;
+`
+// 自定义check
+const CustomIpt = styled.input`
+    text-align: center;
+    width: 40px;
+    border: none; 
+    -webkit-appearance: none;
+    appearance: none;
+
+    :after {
+        content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="#ededed" stroke-width="3"/></svg>');
+    }
+
+    :checked:after {
+        content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="#bddad5" stroke-width="3"/><path fill="#5dc2af" d="M72 25L42 71 27 56l-4 4 20 20 34-52z"/></svg>');
+    }
+`
+
+// 自定义文本框
+const CustomText = styled.span`
+    width: 100%;
+    word-wrap: break-word;
+    position: relative;
+    max-width: 450px;
+    font-size: 20px;
+    font-family: inherit;
+    font-weight: inherit;
+    line-height: 1.4em;
+    border: 0;
+    outline: none;
+    color: inherit;
+    -webkit-font-smoothing: antialiased;
+    -moz-font-smoothing: antialiased;
+    font-smoothing: antialiased;
+    white-space: normal;
+    //同级的就用这种写法
+    &.checked {
+        text-decoration:line-through;
+        color: rgb(180, 177, 177);
+    }
+`
+
+//自定义Newtodo
+const NewTodo = styled.div`
+    margin: 0;
+    list-style: none;
+    padding-left: 16px;
+    border: none;
+    display: inline;
+
+
+    input[type="checkbox"] {
+        /* width: 15%; */
+        /* height: 15%; */
+        align-content: center;
+        border: none; /* Mobile Safari */
+    }
+
+    input[type="checkbox"]:before {
+        content: '❯';
+        font-size: 22px;
+        color: #e6e6e6;
+        padding: 10px 16px;
+    }
+
+    input[type="checkbox"]:checked:before {
+        color: #737373;
+    }
+
+    input[type="text"] {
+        position: relative;
+        margin: 0;
+        width: 85%;
+        font-size: 24px;
+        font-family: inherit;
+        font-weight: inherit;
+        line-height: 1.4em;
+        border: 0;
+        outline: none;
+        color: inherit;
+        padding: 16px 0 16px 36px;
+        -webkit-font-smoothing: antialiased;
+        -moz-font-smoothing: antialiased;
+        font-smoothing: antialiased;
+    }
+
+    input[type="text"]::-webkit-input-placeholder {
+        font-style: italic;
+        font-weight: 300;
+        color: #e6e6e6;
+    }
+        
+    input[type="text"]::-moz-placeholder {
+        font-style: italic;
+        font-weight: 300;
+        color: #e6e6e6;
+    }
+        
+    input[type="text"]::placeholder {
+        font-style: italic;
+        font-weight: 300;
+        color: #e6e6e6;
+    }
+`
+//自定义全选框
+const ToggleAll = styled.input`
+
+	-webkit-transform: rotate(90deg);
+	transform: rotate(90deg);
+	-webkit-appearance: none;
+	appearance: none;
+	position: absolute;
+	top: 12px;
+    left: -8px;
+	width: 60px;
+	height: 34px;
+	text-align: center;
+	z-index: 9;
+	border: none; /* Mobile Safari */
+
+    &:before {
+        content: '❯';
+        font-size: 22px;
+        color: #e6e6e6;
+        padding: 10px 27px 10px 27px;
+    }
+
+        &:checked:before {
+        color: #737373;
+    }
+`
+
+const TodoApp = styled.div`
+    background: #fff;
+	        margin: 130px 0 40px 0;
+	position: relative;
+	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2),
+                0 25px 50px 0 rgba(0, 0, 0, 0.1);
+
+    h1 {
+        position: absolute;
+        top: -155px;
+        width: 100%;
+        font-size: 100px;
+        font-weight: 100;
+        text-align: center;
+        color: rgba(175, 47, 47, 0.15);
+        -webkit-text-rendering: optimizeLegibility;
+        -moz-text-rendering: optimizeLegibility;
+        text-rendering: optimizeLegibility;
+    } 
+ `
 export default Todo;
