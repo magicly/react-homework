0b623fab85c726f01dac44ba248aa9b7b430d00e
panghongxu <476961657@qq.com>
2017-11-05 10:20:29 +0800

    11

--- 2-styled-components/panghongxu/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import TodoPageActive from './TodoPage/index' ;
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<TodoPageActive />, document.getElementById('root'));
-registerServiceWorker();
