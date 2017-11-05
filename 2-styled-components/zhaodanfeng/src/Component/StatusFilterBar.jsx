cd4504c02fe0eb8be5bb28b588d6c37d9659319d
zhaodanfeng <zhaodanfeng92@qq.com>
2017-11-05 08:23:34 +0800

    merge 2-todo (#13)
    * 上传第一次作业
    
    * remove .DS_Store
    
    * 修改任务数量显示错误的问题
    
    * 修改不合法代码
    
    * 完成第二次作业
    
    * 修改readme
    
    * 修复问题

---
+++ 2-styled-components/zhaodanfeng/src/Component/StatusFilterBar.jsx
@@ -0,0 +1,88 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const TodoFooter = styled.footer`
+    color: #777;
+    padding: 10px 15px;
+    height: 20px;
+    text-align: center;
+    border-top: 1px solid #e6e6e6;
+    :before {
+        content: '';
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        height: 50px;
+        overflow: hidden;
+        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 8px 0 -3px #f6f6f6, 0 9px 1px -3px rgba(0, 0, 0, 0.2), 0 16px 0 -6px #f6f6f6, 0 17px 2px -6px rgba(0, 0, 0, 0.2);
+    }
+`
+const TodoCountSpan = styled.span`
+    float: left;
+    text-align: left;
+`
+const Strong = styled.strong`
+    font-weight: 300;
+`
+const BtnsUl = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    position: absolute;
+    right: 0;
+    left: 0;
+`
+const BtnLi = styled.li`
+    display: inline;
+`
+const BtnA = styled.a`
+    color: inherit;
+    margin: 3px;
+    padding: 3px 7px;
+    text-decoration: none;
+    border: 1px solid transparent;
+    border-radius: 3px;
+    border-color: ${props => props.checked ? 'rgba(175, 47, 47, 0.2)' : '#ffffff'};
+`
+const ClearBtn = styled.button`
+    float: right;
+    position: relative;
+    line-height: 20px;
+    text-decoration: none;
+    cursor: pointer;
+    position: relative;
+    display: ${props => props.displayStatus ? 'block': 'none'};
+`
+
+class StatusFilterBar extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            checkedCode: false
+        }
+    }
+    
+    handleFilter = (e) => {
+        this.setState({
+            checkedCode: e.currentTarget.id
+        });
+        this.props.handleFilter(e);
+    }
+
+    render() {
+        let filterActions = [{'code': 'all', 'name': '显示全部'}, {'code': 'active', 'name': '未完成'}, {'code': 'completed', 'name': '已完成'}];
+        filterActions = filterActions.map(element => (
+            <BtnLi key={element.code}><BtnA href="javascript:;" checked={element.code === this.state.checkedCode} id={element.code} onClick={this.handleFilter}>{element.name}</BtnA></BtnLi>
+        ));
+        return (
+            <TodoFooter>
+                <TodoCountSpan>还有<Strong>{this.props.todosCount}</Strong>个任务未完成</TodoCountSpan>
+                <BtnsUl>{filterActions}</BtnsUl>
+                <ClearBtn displayStatus={this.props.showClear} onClick={this.props.handleRemove}>清除已完成任务</ClearBtn>
+            </TodoFooter>
+        );
+    }
+}
+
+export default StatusFilterBar;
