847c4935a2306c3870ee9f2f44c251541d00ee7f
yuanchanghai <358881498@qq.com>
2017-11-05 22:02:04 +0800

    再次提交作业

--- 2-styled-components/zhaodanfeng/src/index.js
+++ 2-styled-components/zhaodanfeng/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoListContainer from './Component/TodoListContainer.jsx';
+import Todo from './Todo/Todo.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<TodoListContainer />, document.getElementById('root'));
+ReactDOM.render(<Todo />, document.getElementById('root'));
 registerServiceWorker();
