4c27dc1f2e377d55354b520c51f01c6b56d397f4
unknown <1751295474@qq.com>
2017-11-12 13:34:17 +0800

    第三次作业-路由和登陆-李春霖

---
+++ 3-router/lichunlin/src/Task/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import TaskContainer from './TaskShow/TaskContainer';
+import LoginContainer,{authorize} from'./Login/LoginContainer';
+import PageNotFound from "./Common/PageNotFound";
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch,
+} from 'react-router-dom';
+
+const EntryRouter = () => {// 全站路由入口
+    return(
+        <div>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact={true} path="/login" component={LoginContainer}/>
+                        <PrivateRouter exact={true} path="/" component={TaskContainer}/>
+                        <PrivateRouter path="/All" component={TaskContainer}/>
+                        <PrivateRouter path="/Active" component={TaskContainer}/>
+                        <PrivateRouter path="/Completed" component={TaskContainer}/>
+                        <PrivateRouter path="/:otherPath" component={PageNotFound}/>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    )
+}
+
+const PrivateRouter = ({component:Component,...rest}) => {// 需要登陆
+    return (
+        <Route {...rest} render={
+            (props) => {
+                let pathname = props.location.pathname;
+
+                if (pathname === "/") {
+                    return <Redirect to="/All"/>
+                }
+                if (authorize.hasLogin()) {
+                    return <Component {...props}/>
+                }
+                return <Redirect to={{
+                    pathname:"/login",
+                    state:{from:props.location}
+                }}/>
+            }
+        }></Route>
+    )
+}
+
+export default EntryRouter;
---
+++ 3-router/lichunlin/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TaskContainer from './Task';
+
+ReactDOM.render(<TaskContainer/>, document.getElementById('root'));
