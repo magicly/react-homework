0ee7585bd277dd8fad0c5911fc10868999d83259
qintao0306 <287736337@qq.com>
2017-11-15 23:48:43 +0800

    秦涛第四次作业

---
+++ 4-backend/qintao/README.md
@@ -0,0 +1,21 @@
+
+#安装说明
+
+##1、代码下载到本地文件中
+```
+$ git clone https://github.com/qintao0306/react-homework
+```
+##2、进入当前文件夹
+```
+$ cd 3-router
+$ cd qintao
+```
+
+##3、安装依赖包
+```
+npm install
+```
+##4、启动应用
+```
+$ npm start
+```
---
+++ 4-backend/qintao/src/Todo/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch
+} from 'react-router-dom'
+import {TodosAll,TodosActive,TodosComplet} from './Todos.js';
+import Login from './Login.js';
+import Logout from './Logout.js'
+const Index = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Private exact path="/" component={TodosAll}/>
+                    <Private exact path="/all" component={TodosAll}/>
+                    <Private exact path="/active"  component={TodosActive} />
+                    <Private exact path="/complet" component={TodosComplet} />
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/logout" component={Logout}/>
+                    <Route render={()=>{
+                        return <div><h1>404 未找到该页面 </h1><br/><Link to="/">进入首页</Link></div>
+                    }} />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
+
+const Private = ({component: Component, ...rest}) => {
+    return <Route {...rest} render={props => {
+        if (localStorage.getItem('haslogin')==='login') {
+          return <Component {...props} />
+        }
+        return <Redirect to={{
+          pathname: '/login',
+          state: { from: props.location }
+        }} />
+      }} />
+}
+
+
+export default Index;
