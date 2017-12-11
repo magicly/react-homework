034556bd5db22e9e906d330a22bfeb6110fbee15
magicly <magicly007@gmail.com>
2017-12-11 11:02:56 +0800

    hello redux

--- redux-demo/src/App.js
+++ redux-demo/src/App.js
@@ -2,7 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state + 1;
+    case 'MINUS':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.store = createStore(reducer);
+    this.store.subscribe(() => {
+      console.log(this.store.getState());
+      this.setState({
+        value: this.store.getState(),
+      })
+    })
+  }
+  state = { value: 0 };
+  add = () => {
+    // this.setState({
+    //   value: this.state.value + 1,
+    // });
+    this.store.dispatch({
+      type: 'ADD',
+    })
+  }
+  minus = () => {
+    this.store.dispatch({
+      type: 'MINUS',
+    })
+    // this.setState({
+    //   value: this.state.value - 1,
+    // });
+  }
+  nothing = () => {
+    this.store.dispatch({
+      type: 'Nothing',
+    })
+    // this.setState({
+    //   value: this.state.value - 1,
+    // });
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +61,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{this.state.value}</p>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+        <button onClick={this.nothing}>Nothing</button>
       </div>
     );
   }
