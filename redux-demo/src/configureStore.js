f8c3effa2a879320f3fb335c4299aa42b6da1cd9
magicly <magicly007@gmail.com>
2017-12-17 12:06:19 +0800

    redux applyMiddleware

--- redux-demo/src/configureStore.js
+++ redux-demo/src/configureStore.js
@@ -1,46 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import logger from 'redux-logger';
 import todoReducers from './reducers';
 
-const logger = store => next => {
-  if (!console.group) {
-    return next;
-  }
-  return action => {
-    console.group(action.type);
-    console.log('%c previous state: ', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = next(action);
-    console.log('%c next state', 'color: red', store.getState());
-    console.groupEnd(action.type);
-
-    return returnValue;
-  }
-}
-
-const promise = store => next => action => {
-  if (action.constructor.name === 'Promise') {
-    return action.then(next);
-  }
-  return next(action);
-}
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware => {
-    store.dispatch = middleware(store)(store.dispatch);
-  });
-}
 export default () => {
-  const store = createStore(todoReducers);
-
-  const middlewares = [];
-
-  middlewares.push(promise);
-
+  const middlewares = [promise];
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(logger);
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares);
+  const store = createStore(
+    todoReducers,
+    applyMiddleware(...middlewares)
+  );
 
   if (process.env.NODE_ENV !== "production") {
     if (module.hot) {
